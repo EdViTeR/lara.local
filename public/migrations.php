0720490edae855b744ffbabb5c28e4117cd1a911
EdViTeR <maxpivnnko@mail.ru>
2025-03-05 21:09:54 +0300

    migrations 72

--- public/migrations.php
+++ public/migrations.php
@@ -36,3 +36,25 @@ Schema::table('user', function(Blueprint $table) {
     $table->renameColumn('surname', 'second_name');
 });
 ///////////////////////////////////////////////////////////
+// ⊗pplrPmMgFM_68 №1-4
+Schema::table('user', function(Blueprint $table) {
+    $table->string('email')->comment('Это почта')->change();
+    $table->integer('salary')->default(0)->change();
+    $table->integer('age')->nullable()->unsigned()->change();
+});
+///////////////////////////////////////////////////////////
+// ⊗pplrPmMgFO_69 №1 №2
+Schema::table('user', function(Blueprint $table) {
+    $table->string('first_name')->first()->change();
+    $table->string('sex')->after('id');
+});
+///////////////////////////////////////////////////////////
+// ⊗pplrPmMgTRm_70 №1
+Schema::dropIfExists('user');
+// №2
+    // Schema::drop('userq'); //выведет ошибку
+///////////////////////////////////////////////////////////
+// ⊗pplrPmMgTRn_71 №1
+Schema::rename('user', 'users');
+///////////////////////////////////////////////////////////
+// ⊗pplrPmMgRBP_72 №1
